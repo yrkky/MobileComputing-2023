cc1099dfdf1f56acfca96381d3297a4acabca97d
Janne Yrjänäinen <janne.yrjanainen@gmail.com>
2023-02-21 00:20:45 +0200

    fix errors

--- app/src/main/java/com/yrkky/mobilecomp/ui/reminder/ReminderList.kt
+++ app/src/main/java/com/yrkky/mobilecomp/ui/reminder/ReminderList.kt
@@ -2,7 +2,6 @@ package com.yrkky.mobilecomp.ui.reminder
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.BoxScopeInstance.align
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
@@ -90,7 +89,6 @@ private fun ReminderListItem(
         Icon(
             imageVector = nameToIcon(reminder.icon),
             contentDescription = null,
-            modifier = Modifier.
         )
 
         Text(
